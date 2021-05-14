ef6d5e67c614afda8c053e7a13c961898d47d14c
betul-sahin <betulsahin3387@hotmail.com>
2021-05-14 16:24:26 +0300

    Initial project with Spring initializr

---
+++ src/main/java/com/betulsahin/filmkoleksiyonuapp/FilmKoleksiyonuAppApplication.java
@@ -0,0 +1,13 @@
+package com.betulsahin.filmkoleksiyonuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmKoleksiyonuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmKoleksiyonuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betulsahin/filmkoleksiyonuapp/FilmKoleksiyonuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betulsahin.filmkoleksiyonuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmKoleksiyonuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
